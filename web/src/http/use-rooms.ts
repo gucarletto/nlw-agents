2c272133b1432a1b8a178e045fc6198e471ba941
Gustavo Carletto <gucarletto@gmail.com>
2025-07-09 09:00:07 -0300

    feat: pre class 3 fixe

--- web/src/http/use-rooms.ts
+++ web/src/http/use-rooms.ts
@@ -1,14 +1,14 @@
-import { useQuery } from "@tanstack/react-query"
-import type { GetRoomsAPIResponse } from "./types/get-rooms-response"
+import { useQuery } from '@tanstack/react-query'
+import type { GetRoomsResponse } from './types/get-rooms-response'
 
 export function useRooms() {
   return useQuery({
     queryKey: ['get-rooms'],
     queryFn: async () => {
       const response = await fetch('http://localhost:3333/rooms')
-      const result: GetRoomsAPIResponse = await response.json()
+      const result: GetRoomsResponse = await response.json()
 
       return result
-    }
+    },
   })
 }
